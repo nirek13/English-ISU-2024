292f72dbce7f7c1f2ff35b392f09709b7be18026
nirek13 <nirekshetty@gmail.com>
2024-05-26 12:06:43 -0400

    global themes added

--- assets/JS/theme.js
+++ assets/JS/theme.js
@@ -1,20 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     const logo = document.getElementById('logo');
+    let currentTheme;
+
+    // Retrieve the stored theme preference from localStorage
+    const storedTheme = localStorage.getItem('theme');
+
+    // Set the currentTheme variable based on the stored theme preference or default to 'theme1'
+    if (storedTheme) {
+        currentTheme = storedTheme;
+    } else {
+        currentTheme = 'theme1';
+    }
+
+    // Apply the current theme
+    document.documentElement.setAttribute('data-theme', currentTheme);
 
     logo.addEventListener('click', () => {
         console.log('Button clicked!');
 
-        // Add your theme switching logic here
-        // For example, you can toggle a data attribute on the <html> element
-        const currentTheme = document.documentElement.getAttribute('data-theme');
-        console.log(`current theme ${currentTheme}`)
+        // Toggle theme logic
         let newTheme;
         if (currentTheme === 'theme1') {
             newTheme = 'light';
         } else if (currentTheme === 'light') {
             newTheme = 'theme2';
-        }
-        else if (currentTheme === 'theme2') {
+        } else if (currentTheme === 'theme2') {
             newTheme = 'theme3';
         } else if (currentTheme === 'theme3') {
             newTheme = 'theme4';
@@ -23,6 +33,14 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             newTheme = 'theme1';
         }
+
+        // Update the currentTheme variable and store the new theme preference in localStorage
+        currentTheme = newTheme;
+        localStorage.setItem('theme', currentTheme);
+
+        console.log(`Current theme: ${currentTheme}`);
+        // Apply the new theme
         document.documentElement.setAttribute('data-theme', newTheme);
     });
 });
+
