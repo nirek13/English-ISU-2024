dde1b51c26e1a2195c5f60898260dc556e4202d5
nirek13 <nirekshetty@gmail.com>
2024-05-26 11:01:41 -0400

    giddy up

--- assets/JS/theme.js
+++ assets/JS/theme.js
@@ -3,13 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     logo.addEventListener('click', () => {
         console.log('Button clicked!');
+
         // Add your theme switching logic here
         // For example, you can toggle a data attribute on the <html> element
         const currentTheme = document.documentElement.getAttribute('data-theme');
+        console.log(`current theme ${currentTheme}`)
         let newTheme;
         if (currentTheme === 'theme1') {
+            newTheme = 'light';
+        } else if (currentTheme === 'light') {
             newTheme = 'theme2';
-        } else if (currentTheme === 'theme2') {
+        }
+        else if (currentTheme === 'theme2') {
             newTheme = 'theme3';
         } else if (currentTheme === 'theme3') {
             newTheme = 'theme4';
